49c31c1b3c7ea00ab84a3b40207f9b5bd4dd82c6
mindula-madhuhansa <mimmindula@gmail.com>
2024-04-07 18:23:36 +0530

    add header

---
+++ components/Header.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { PlusCircle } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { navLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+
+const Header = () => {
+  const pathname = usePathname();
+
+  return (
+    <header className="fixed top-0 flex items-center justify-between w-full z-10 bg-gray-50 p-6 px-10 shadow-sm">
+      <Link href="/" className="flex items-center gap-3">
+        <Image
+          src="/images/logo.png"
+          alt="Logo"
+          width={64}
+          height={64}
+          className="object-cover"
+        />
+        <h1 className="hidden sm:flex text-2xl font-semibold uppercase">
+          Property Sell
+        </h1>
+      </Link>
+
+      <nav className="hidden md:flex gap-10 text-sm font-medium">
+        {navLinks.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={cn(
+              "text-muted-foreground hover:text-primary",
+              pathname === link.href && "text-primary"
+            )}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </nav>
+
+      <div className="flex items-center gap-3">
+        <Button className="flex gap-2">
+          <span>Post Your Ad</span>
+          <PlusCircle className="h-5 w-5" />
+        </Button>
+        <Button variant="outline">Login</Button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+export const navLinks = [
+  {
+    label: "Buy",
+    href: "/",
+  },
+  {
+    label: "Sell",
+    href: "/",
+  },
+  {
+    label: "Find Agent",
+    href: "/",
+  },
+];
